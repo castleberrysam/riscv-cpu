3659a89f9735b29464dd14c0e896e8ce6e7d3dc5
Charles Zhang <charleszhang99@yahoo.com>
2020-05-23 21:54:54 -0700

    Add two forwarding related tests.

---
+++ test/forward.s
@@ -0,0 +1,14 @@
+	.text
+	addi    t5, x0, 1
+	addi	s0, t5, 60
+	addi    s0, t5, 3
+	addi    t0, s0, 1
+	addi    s0, s0, 1
+	addi    s0, s0, 1
+	add     s0, s0, s0
+	add     s0, t5, s0
+	add     t5, s0, t5
+	add     t5, t5, t5
+	addi    x0, x0, -12
+	add     s0, x0, x0
+	.word	0
---
+++ test/load-store-hazard.s
@@ -0,0 +1,11 @@
+	.text
+	addi	s0, t5, 60
+	auipc   s0, 0
+	sw	t0, 0(s0)
+	lh	t1, 2(s0)
+	addi    t3, t1, 3
+	lh	a0, 0(s0)
+	add     t4, t1, a0
+	lb	s1, 1(s0)
+	sub     t1, t3, s0
+	.word	0
